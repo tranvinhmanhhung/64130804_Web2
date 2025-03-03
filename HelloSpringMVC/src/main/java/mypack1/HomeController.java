3b730e30f155c02ce41378d24634b458b0d58f2b
tranvinhmanhhung <tranvinhmanhhung@gmail.com>
2025-03-03 23:47:20 +0700

    SpringMVC

--- HelloSpringMVC/src/main/java/mypack1/HomeController.java
+++ HelloSpringMVC/src/main/java/mypack1/HomeController.java
@@ -1,4 +1,16 @@
 package mypack1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
 public class HomeController {
+
+    @RequestMapping("/hello")
+
+    public ModelAndView getPage() {
+        ModelAndView m = new ModelAndView();
+        m.setViewName("welcome");
+        return m;
+    }
 }
