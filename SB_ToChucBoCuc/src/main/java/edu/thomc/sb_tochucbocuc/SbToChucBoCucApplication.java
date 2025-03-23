9eddf02521fc3896aaeaf297dae1c6234ae1f16c
tranvinhmanhhung <tranvinhmanhhung@gmail.com>
2025-03-23 18:07:08 +0700

    Boostrap ToChucBoCuc

---
+++ SB_ToChucBoCuc/src/main/java/edu/thomc/sb_tochucbocuc/SbToChucBoCucApplication.java
@@ -0,0 +1,13 @@
+package edu.thomc.sb_tochucbocuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbToChucBoCucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbToChucBoCucApplication.class, args);
+	}
+
+}
---
+++ SB_ToChucBoCuc/src/test/java/edu/thomc/sb_tochucbocuc/SbToChucBoCucApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.thomc.sb_tochucbocuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbToChucBoCucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
