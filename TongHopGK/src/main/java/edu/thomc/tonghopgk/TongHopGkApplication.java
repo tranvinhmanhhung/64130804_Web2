68bcfe03a9ffdbc1acc0e5b79acd3a4229f77fa9
tranvinhmanhhung <tranvinhmanhhung@gmail.com>
2025-03-25 09:14:31 +0700

    TongHopGK

---
+++ TongHopGK/src/main/java/edu/thomc/tonghopgk/TongHopGkApplication.java
@@ -0,0 +1,13 @@
+package edu.thomc.tonghopgk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongHopGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongHopGkApplication.class, args);
+	}
+
+}
---
+++ TongHopGK/src/test/java/edu/thomc/tonghopgk/TongHopGkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.thomc.tonghopgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
