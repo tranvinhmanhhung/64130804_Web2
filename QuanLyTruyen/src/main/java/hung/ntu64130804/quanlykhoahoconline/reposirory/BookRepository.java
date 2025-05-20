6cfdcb63ae80cf1c40c76fdb0766c2fa1d043a61
tranvinhmanhhung <tranvinhmanhhung@gmail.com>
2025-05-20 09:46:45 +0700

    cap nhat giao dien chua chuc nang

--- QuanLyTruyen/src/main/java/hung/ntu64130804/quanlykhoahoconline/reposirory/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package hung.ntu64130804.quanlykhoahoconline.reposirory;
-
-import hung.ntu64130804.quanlykhoahoconline.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
